9916806482fccc04d36430abbaa62144a1a5e7ea
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-11-28 15:46:03 +0100

    Increase retries

--- src/glvd/util/requests.py
+++ src/glvd/util/requests.py
@@ -10,8 +10,8 @@ class RetrySession(requests.Session):
         super().__init__()
         adapter = requests.adapters.HTTPAdapter(
             max_retries=urllib3.Retry(
-                total=10,
-                backoff_factor=5,
+                total=30,
+                backoff_factor=10,
                 allowed_methods=None,
                 status_forcelist=[429, 500, 502, 503, 504],
             ),
