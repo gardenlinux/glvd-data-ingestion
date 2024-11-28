6df7e144e3f2cf19853829f11409771237f79cc0
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-11-28 14:25:44 +0100

    Retry more often

--- src/glvd/util/requests.py
+++ src/glvd/util/requests.py
@@ -10,8 +10,8 @@ class RetrySession(requests.Session):
         super().__init__()
         adapter = requests.adapters.HTTPAdapter(
             max_retries=urllib3.Retry(
-                total=5,
-                backoff_factor=1,
+                total=10,
+                backoff_factor=5,
                 allowed_methods=None,
                 status_forcelist=[429, 500, 502, 503, 504],
             ),
