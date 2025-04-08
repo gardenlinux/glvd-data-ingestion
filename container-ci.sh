0b968fb9cb40d6b46497a06a802418456878d3a9
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-04-08 14:38:06 +0200

    test

--- container-ci.sh
+++ container-ci.sh
@@ -5,6 +5,5 @@ set -x
 
 psql glvd -f /usr/local/src/schema/glvd-db-schema.sql
 /usr/local/src/ingest-postgres.sh
-psql glvd -f /usr/local/src/extra-schema.sql
 pg_dump --schema-only -U glvd glvd
 pg_dump -U glvd glvd > /tmp/glvd-dump.sql
