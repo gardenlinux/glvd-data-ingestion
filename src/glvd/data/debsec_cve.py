6752214d0b613d23d1ea95fbca49a9e7148d9d6f
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-05-23 15:29:27 +0200

    Filter out TEMP- and other non-CVE entries

--- src/glvd/data/debsec_cve.py
+++ src/glvd/data/debsec_cve.py
@@ -12,21 +12,23 @@ class DebsecCveFile(dict[str, dict[tuple[str, str], DebsecCve]]):
     def read(self, f: dict) -> None:
         for package, cves in f.items():
             for cve_id, entry in cves.items():
-                for release in entry['releases']:
-                    if release != 'sid': 
-                        codename = release
-                        tag = entry.get("tag")
-                        version_fixed = entry['releases'][codename].get('fixed_version')
-                        note = entry.get("note")
-                        per_codename = self.setdefault(codename, {})
-                        per_codename[(cve_id, package)] = DebsecCve(
-                            cve_id=cve_id,
-                            dist=None,
-                            deb_source=package,
-                            deb_version_fixed=version_fixed,
-                            debsec_tag=tag,
-                            debsec_note=note,
-                        )
+                # Filter out TEMP- and other non-CVE entries
+                if not cve_id.startswith('CVE-'):
+                    for release in entry['releases']:
+                        if release != 'sid': 
+                            codename = release
+                            tag = entry.get("tag")
+                            version_fixed = entry['releases'][codename].get('fixed_version')
+                            note = entry.get("note")
+                            per_codename = self.setdefault(codename, {})
+                            per_codename[(cve_id, package)] = DebsecCve(
+                                cve_id=cve_id,
+                                dist=None,
+                                deb_source=package,
+                                deb_version_fixed=version_fixed,
+                                debsec_tag=tag,
+                                debsec_note=note,
+                            )
 
 
 if __name__ == '__main__':
