190b707533b62667c53501e75e248511970feba9
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2025-05-28 14:48:06 +0200

    Bootstrap pytest infrastructure (#39)
    * Bootstrap pytest infrastructure
    
    * Run unit tests in container
    
    * separate test and image build
    
    * Add trivial test for debsrc

---
+++ tests/unit/test_debsrc.py
@@ -0,0 +1,16 @@
+import pytest
+
+from glvd.data.debsrc import DebsrcFile, Debsrc
+
+def test_can_parse_debsrc_file():
+    expected = {
+        'glibc': Debsrc(deb_source='glibc',
+                        deb_version='2.41-6',
+                        dist=None)
+    }
+
+    actual = DebsrcFile()
+    with open('tests/unit/package-source-list') as f:
+        actual.read(f)
+
+    assert actual == expected
---
+++ unit-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=src pytest -vv tests/unit/
