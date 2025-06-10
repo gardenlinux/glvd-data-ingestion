3a16dde6a85a35ea936c029cb0c286c2ef04a44f
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2025-06-10 14:18:58 +0200

    Enable pg-formatter check again (#43)
    By downloading the pgformatter tarball using a token we should be fine with rate limits.
    
    Fixes #40

--- format-sql-schema.sh
+++ format-sql-schema.sh
@@ -2,6 +2,10 @@
 set -e
 set -x
 
+if [ ! -f pgformatter.tgz ]; then
+    curl -H "Authorization: Bearer $GITHUB_TOKEN" -L -o pgformatter.tgz https://github.com/darold/pgFormatter/archive/refs/tags/v5.6.tar.gz
+fi
+
 podman build -t localhost/pg-formatter:latest -f Containerfile.pg-formatter .
 
 find schema -iname "*.sql" -print0 | while IFS= read -r -d '' file; do
