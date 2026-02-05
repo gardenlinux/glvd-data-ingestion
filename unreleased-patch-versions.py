b203825c0264cc1b96027eb6efdd86cc560e44f5
Moritz Kirchner <moritz.kirchner@sap.com>
2026-02-05 14:53:44 +0100

    Adapted the script to handle .0 versions that were introduced in 1877 (#107)
    This is needed to handle Garden Linux versions >= 2017
    
    ---------
    
    Co-authored-by: Florian Wilhelm <florian.wilhelm02@sap.com>

--- unreleased-patch-versions.py
+++ unreleased-patch-versions.py
@@ -1,20 +1,50 @@
 import sys
 
-def get_next_unreleased_versions(version_string):
-    versions = version_string.split()
-    major_versions = {}
 
-    for version in versions:
-        major, minor = map(int, version.split('.'))
-        if major not in major_versions:
-            major_versions[major] = minor
+def get_next_unreleased_versions(version_string: str) -> str:
+    """Get one long string with many Garden Linux Versions included
+        find the next minor version for each major version.
+
+    >>> get_next_unreleased_versions("1592.4")
+    '1592.5'
+    >>> get_next_unreleased_versions("1877.10.0")
+    '1877.11'
+    >>> get_next_unreleased_versions("2345.0")
+    '2345.1.0'
+    >>> get_next_unreleased_versions("2345.0.0")
+    '2345.1.0'
+    >>> get_next_unreleased_versions("1877.10 1877.9 1592.4 1592.3 2345.1.0")
+    '1592.5 1877.11 2345.2.0'
+
+    """
+
+    garden_linux_versions = version_string.split()
+    versions = {}
+
+    for version in garden_linux_versions:
+        major, minor = map(int, version.split(".")[:2])
+
+        if major not in versions:
+            versions[major] = minor
         else:
-            major_versions[major] = max(major_versions[major], minor)
+            versions[major] = max(versions[major], minor)
 
-    next_versions = [f"{major}.{minor + 1}" for major, minor in sorted(major_versions.items())]
-    return ' '.join(next_versions)
+    next_versions = []
+    for major, minor in sorted(versions.items()):
+        next_version = f"{major}.{minor + 1}"
+        # releases after 2017 should be triple digit versions, e.g. 2017.0.0
+        # https://github.com/gardenlinux/gardenlinux?tab=readme-ov-file#-important-
+        if major >= 2017:
+            next_version += ".0"
+        next_versions.append(next_version)
+
+    return " ".join(next_versions)
+
 
 if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
     if len(sys.argv) != 2:
         print("Usage: python unreleased-patch-versions.py '<version_string>'")
         sys.exit(1)
